7185b2739da0a1105c41a65d792241b7e2407ede
ntazewell <ntazewell@drew.edu>
2015-09-14 10:17:16 -0400

    Added NumSides

--- Shapes/src/Circle.java
+++ Shapes/src/Circle.java
@@ -2,10 +2,14 @@
 public class Circle  {
 
 private int radius;
+//number of sides
+private double ns;	
+
 	public Circle() {
 	
 		radius = 2;
-	
+		ns = 0;
+		//since circles have no sides
 	}
   public double getWidth(){
 	  return radius* 2;
@@ -15,6 +19,13 @@ private int radius;
   public double getHeight(){
 	  return radius *2;
   }
+  
+  public double getNumSides()
+  {
+	  return ns;
+  }
+  
+  
   public static void main(String [] args){
 	  Circle c = new Circle();
 	  System.out.println(c.getWidth());
